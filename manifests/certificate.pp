b79081fa6bef562aeccb1816477c8551e463617a
deigote <diego@toharia.com>
2012-06-05 09:19:18 +0200

    The deps for certificate now includes the cert parent directory

--- manifests/certificate.pp
+++ manifests/certificate.pp
@@ -2,21 +2,23 @@ define apache2_ssl::certificate($cert_name, $cert_file_source, $cert_key_file_so
 
     include apache2_ssl
 
-    file { "$apache2_ssl::apache2_ssl_dir/$cert_name.pem":
+    $cert_dir = $apache2_ssl::apache2_ssl_dir
+
+    file { "$cert_dir/$cert_name.pem":
         ensure => present,
         owner => 'root',
         group => 'root',
         mode => 0644,
 	source => "$cert_file_source",
-        require => File["$apache2_ssl_dir"]
+        require => File["$cert_dir"]
     }
 
-    file { "$apache2_ssl::apache2_ssl_dir/$cert_name.key":
+    file { "$cert_dir/$cert_name.key":
         ensure => present,
         owner => 'root',
         group => 'root',
         mode => 0644,
 	source => "$cert_key_file_source",
-        require => File["$apache2_ssl_dir"]
+        require => File["$cert_dir"]
     }
 }
